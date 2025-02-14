500ba999153455617488808fb99604e622819a64
kolodkinds <baranovskijivan76@gmail.com>
2025-02-14 19:12:11 +0500

    иконка

---
+++ app/src/main/java/com/example/map2/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.map2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.map2.ui.theme.Map2Theme
+import ru.sulgik.mapkit.MapKit
+import ru.sulgik.mapkit.compose.Placemark
+import ru.sulgik.mapkit.compose.YandexMap
+import ru.sulgik.mapkit.compose.bindToLifecycleOwner
+import ru.sulgik.mapkit.compose.rememberAndInitializeMapKit
+import ru.sulgik.mapkit.compose.rememberCameraPositionState
+import ru.sulgik.mapkit.compose.rememberPlacemarkState
+import ru.sulgik.mapkit.geometry.Point
+import ru.sulgik.mapkit.map.CameraPosition
+import ru.sulgik.mapkit.map.ImageProvider
+import ru.sulgik.mapkit.map.fromResource
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            enableEdgeToEdge()
+            initMapKit()
+            MapScreen()
+
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+val startPosition = CameraPosition(Point(63.723768, 75.961573),11.0f,10.0F,0.0f)
+
+val placemarkGeometry = Point(63.201436, 75.451114)
+@Composable
+fun MapScreen() {
+
+    val context = LocalContext.current
+
+    rememberAndInitializeMapKit().bindToLifecycleOwner() // if is not called earlier
+    val cameraPositionState = rememberCameraPositionState { position = startPosition }
+    YandexMap(
+        cameraPositionState = cameraPositionState,
+        modifier = Modifier.fillMaxSize(),
+    ){
+
+
+
+        Placemark(state = rememberPlacemarkState(placemarkGeometry), icon = ImageProvider.fromResource(context = context, R.drawable.mappp),)
+
+    }
+
+}
+
+
+
+@Composable
+fun initMapKit() {
+    MapKit.setApiKey("299e664a-6317-4736-9d0b-941426428ecd")
+}
